00b534c84763c7d33fbdd87304f55733d725cc44
Lfw08 <liufw666@outlook.com>
2023-12-01 22:18:11 +0800

    First Commit!
    Migrated Lfw08.github.io to shsbnu.github.io

---
+++ Untitled-1.bat
@@ -0,0 +1,7 @@
+echo "# lfw08.github.io" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Lfw08/lfw08.github.io.git
+git push -u origin main
